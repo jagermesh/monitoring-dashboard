8140f531c10b4958da80eef0a13127e757d709fa
Sergiy Lavryk <jagermesh@gmail.com>
2020-09-22 23:27:28 +0300

    fx

--- www/js/renderers/Renderer_Custom.js
+++ www/js/renderers/Renderer_Custom.js
@@ -4,23 +4,19 @@ function Renderer_Custom(container, sensorInfo, metricInfo, settings) {
 
   const widgetTemplate = Handlebars.compile(`
     <div class="widget card mb-3 mr-3" data-ip="{{sensorInfo.address}}" data-uid="{{metricInfo.uid}}" data-metric-name="{{metricInfo.name}}" data-renderer-name="{{metricInfo.rendererName}}">
-      <div class="card-header pl-2 pt-1 pb-1 pr-2">
-        <div class="widget-header">
-          <button type="button" class="close widget-action-close" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-          <div class="widget-title">
-          </div>
-          <div class="widget-sub-title">
-          </div>
+      <div class="widget-header card-header pl-2 pt-1 pb-1 pr-2">
+        <button type="button" class="close widget-action-close" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <div class="widget-title">
+        </div>
+        <div class="widget-sub-title">
         </div>
       </div>
-      <div class="card-body m-0 p-1">
-        <div class="widget-body">
-        </div>
+      <div class="widget-body card-body m-0 p-1">
       </div>
-      <div class="card-footer pl-2 pt-1 pb-1 pr-2">
-        <div class="widget-footer">
+      <div class="widget-footer card-footer pl-2 pt-1 pb-1 pr-2">
+        <div class="widget-end-title">
           {{sensorInfo.address}}
         </div>
       </div>

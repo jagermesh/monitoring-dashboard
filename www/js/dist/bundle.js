1c200b1f9d71b4863b1fe770ef535bb1d1ebb802
Sergiy Lavryk <jagermesh@gmail.com>
2020-10-07 16:16:17 +0300

    fx

--- www/js/dist/bundle.js
+++ www/js/dist/bundle.js
@@ -8712,7 +8712,6 @@ class CustomRenderer {
           <button type="button" class="close widget-action-close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
           <div class="widget-title">
             <span class="widget-title-info"></span>
-            <span class="widget-title-tags badge badge-pill badge-warning">{{metricInfo.metricTags}}</span>
           </div>
           <div class="widget-sub-title"></div>
         </div>
@@ -8722,9 +8721,11 @@ class CustomRenderer {
           <div class="widget-footer-title float-left">
             {{sensorInfo.sensorLocation}}
           </div>
+          {{#if metricInfo.metricTags}}
           <div class="widget-footer-tags float-right">
-            <span class="widget-title-tags badge badge-pill badge-warning">{{metricInfo.metricTags}}</span>
+            {{metricInfo.metricTags}}
           </div>
+          {{/if}}
           <div class="widget-footer-sub-title float-right">
             {{metricConfig.settings}}
           </div>
@@ -8737,11 +8738,11 @@ class CustomRenderer {
 
     this.control_Title = this.widgetContainer.find('.widget-title');
     this.control_TitleInfo = this.control_Title.find('.widget-title-info');
-    this.control_TitleTags = this.control_Title.find('.widget-title-tags');
     this.control_SubTitle = this.widgetContainer.find('.widget-sub-title');
     this.control_Body = this.widgetContainer.find('.widget-body');
     this.control_Footer_Title = this.widgetContainer.find('.widget-footer-title');
     this.control_Footer_SubTitle = this.widgetContainer.find('.widget-footer-sub-title');
+
   }
 
   pushData(data) {
